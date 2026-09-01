4ea8981b1e02c47d5d236418cc1f03595157f6ef
ZiaKahilaDagna <zia.kahila5@smk.belajar.id>
2026-09-01 13:19:35 +0700

    update route porto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\PortofolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/portfolios', [PortfolioController::class, 'index']) ->name('portfolios.index');
-Route::get('/portfolios/create', [PortfolioController::class, 'create']) ->name('portfolios.create');
-Route::post('/portfolios', [PortfolioController::class, 'store']) ->name('portfolios.store');
-Route::get('/portfolios/{portfolio}', [PortfolioController::class, 'show']) ->name('portfolios.show');
-Route::get('/portfolios/{portfolio}/edit', [PortfolioController::class, 'edit']) ->name('portfolios.edit');
-Route::put('/portfolios/{portfolio}', [PortfolioController::class, 'update']) ->name('portfolios.update');
-Route::delete('/portfolios/{portfolio}', [PortfolioController::class, 'destroy']) ->name('portfolios.destroy');
+Route::get('/portfolios', [PortofolioController::class, 'index'])->name('portfolios.index'); 
+Route::get('/portfolios/create', [PortofolioController::class, 'create'])->name('portfolios.create'); 
+Route::post('/portfolios', [PortofolioController::class, 'store'])->name('portfolios.store'); 
+Route::get('/portfolios/{id}/edit', [PortofolioController::class, 'edit'])->name('portfolios.edit'); 
+Route::put('/portfolios/{id}', [PortofolioController::class, 'update'])->name('portfolios.update'); 
+Route::delete('/portfolios/{id}', [PortofolioController::class, 'destroy'])->name('portfolios.destroy'); 
+Route::get('/portfolios/{id}', [PortofolioController::class, 'show'])->name('portfolios.show'); 
