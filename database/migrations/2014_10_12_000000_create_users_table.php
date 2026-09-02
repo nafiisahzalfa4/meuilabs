8e96fb668d6c21ad3e601d364191102c005e9ec7
nafiisahzalfa4 <zalfaa.nafiisah28@smk.belajar.id>
2026-09-02 13:21:56 +0700

    betulin view

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,25 +4,40 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    public function up(): void
+    /**
+     * Run the migrations.
+     */
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+
             $table->string('name', 100);
             $table->string('email', 100)->unique();
             $table->string('password');
-            $table->enum('role', ['admin','customer','designer'])->default('customer');
-            $table->string('no_telepon', 20)->nullable();
+
+            $table->enum('role', [
+                'admin',
+                'customer',
+                'designer'
+            ])->default('customer');
+
+            $table->string('no_telepon', 20)->nullable(); 
             $table->text('alamat')->nullable();
             $table->string('foto')->nullable();
+
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    public function down(): void
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,6 +1,14 @@
 <h2>Tambah User</h2>
 
-<form action="{{ route('user.store') }}" method="POST">
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+<form action="{{ route('user.store') }}" method="POST" enctype="multipart/form-data">
 
     @csrf
 
@@ -14,6 +22,16 @@
 
     <br><br>
 
+    <label>Password</label><br>
+    <input type="password" name="password">
+
+    <br><br>
+
+    <label>Konfirmasi Password</label><br>
+    <input type="password" name="password_confirmation">
+
+    <br><br>
+
     <label>Role</label><br>
     <select name="role">
         <option value="">-- Pilih Role --</option>
@@ -21,6 +39,21 @@
         <option value="customer">Customer</option>
         <option value="designer">Designer</option>
     </select>
+
+    <br><br>
+
+    <label>No. Telepon</label><br>
+    <input type="text" name="no_telepon">
+
+    <br><br>
+
+    <label>Alamat</label><br>
+    <textarea name="alamat"></textarea>
+
+    <br><br>
+
+    <label>Foto</label><br>
+    <input type="file" name="foto">
 
     <br><br>
 
